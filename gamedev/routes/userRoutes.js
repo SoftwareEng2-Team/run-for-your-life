dee7e109178f20392300c6195ce76cfaca4cf39a
btmunger <btmunger@gmail.com>
2025-03-06 19:12:39 -0800

    Added debugging statement in userRoutes.js

--- gamedev/routes/userRoutes.js
+++ gamedev/routes/userRoutes.js
@@ -3,7 +3,10 @@ import { registerUser, loginUser } from '../controllers/userController.js';
 
 const router = express.Router();
 
+console.log("testing, entered userroutes.js");
+
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+
 
 export default router;
