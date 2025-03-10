85022cb02bb77d455a1b1ea09badd484096f4a9f
Evan Albert <87677067+Evan-Albert@users.noreply.github.com>
2025-03-09 18:01:16 -0700

    Adjusted Jest and added new setup routes.

---
+++ gamedev/jest.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'jest-puppeteer',
+  setupFilesAfterEnv: [
+    '@testing-library/jest-dom',
+    '<rootDir>/jest.setup.js'
+  ],
+  testMatch: ['**/__tests__/**/*.test.js'],
+  globals: {
+    BASE_URL: 'file://' + process.cwd() + '/public_html/'
+  }
+};
